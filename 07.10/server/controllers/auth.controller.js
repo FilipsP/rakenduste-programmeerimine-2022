23f878ed999145f55786d0a5d3fef94f1cbe85e9
Filipp-Artur Pljassunov <33956131+FilipsP@users.noreply.github.com>
2022-10-08 18:05:20 +0300

    07.10 classwork

---
+++ 07.10/server/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+const User = require('../models/user.model');
+
+exports.signup = async (req, res) => {
+  User.signup(req.body)
+    .then((data) => res.send(`Seems to be done ${data}`))
+    .catch((err) => res.send(`Failed ${err}`))
+}
+
+exports.login = async (req, res) => {}
+exports.protected = async (req, res) => {}
